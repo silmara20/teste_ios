1fd1b0bcc7a171effd22ffd08f06bf1371d6c23d
Virtual Machine <virtualmachine@Virtuals-MacBook-Pro.local>
2022-03-18 09:20:50 -0300

    primeiro commit

---
+++ desafio-solutis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  desafio-solutis
+//
+//  Created by Virtual Machine on 18/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
